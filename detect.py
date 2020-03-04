b0f4263904f466dd69208d9a90ff9d9f47430565
“xiaobin1231” <958975859@qq.com>
2020-03-04 17:07:23 +0800

    Fall-Detection-By-YOLOV3-and-LiteFlowNet

---
+++ detect.py
@@ -0,0 +1,232 @@
+import argparse
+from sys import platform
+
+from models import *  # set ONNX_EXPORT in models.py
+from utils.datasets import *
+from utils.utils import *
+
+from LiteFlowNet.run import Network, batch_estimate
+
+
+def detect(save_img=False):
+    img_size = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
+    out, source, weights, half, view_img, save_txt = opt.output, opt.source, opt.weights, opt.half, opt.view_img, opt.save_txt
+    webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
+
+    # Initialize
+    device = torch_utils.select_device(device='cpu' if ONNX_EXPORT else opt.device)
+    if os.path.exists(out):
+        shutil.rmtree(out)  # delete output folder
+    os.makedirs(out)  # make new output folder
+
+    # Initialize model
+    LiteFlowNet = Network()
+    LiteFlowNet.load_state_dict(torch.load(opt.LiteFlowNet_weights))
+    LiteFlowNet.to(device).eval()
+
+    model = Darknet(opt.cfg, img_size)
+
+    # Load weights
+    attempt_download(weights)
+    if weights.endswith('.pt'):  # pytorch format
+        model.load_state_dict(torch.load(weights, map_location=device)['model'])
+    else:  # darknet format
+        load_darknet_weights(model, weights)
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = torch_utils.load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model'])  # load weights
+        modelc.to(device).eval()
+
+    # Fuse Conv2d + BatchNorm2d layers
+    # model.fuse()
+    # torch_utils.model_info(model, report='summary')  # 'full' or 'summary'
+
+    # Eval mode
+    model.to(device).eval()
+
+    # Export mode
+    if ONNX_EXPORT:
+        model.fuse()
+        img = torch.zeros((1, 3) + img_size)  # (1, 3, 320, 192)
+        torch.onnx.export(model, img, 'weights/export.onnx', verbose=False, opset_version=10)
+
+        # Validate exported model
+        import onnx
+        model = onnx.load('weights/export.onnx')  # Load the ONNX model
+        onnx.checker.check_model(model)  # Check that the IR is well formed
+        print(onnx.helper.printable_graph(model.graph))  # Print a human readable representation of the graph
+        return
+
+    # Half precision
+    half = half and device.type != 'cpu'  # half precision only supported on CUDA
+    if half:
+        model.half()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = True
+        torch.backends.cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=img_size, half=half)
+    else:
+        save_img = True
+        dataset = LoadImages(source, img_size=img_size, half=half)
+
+    # Get names and colors
+    names = load_classes(opt.names)
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+
+    # Run inference
+    t0 = time.time()
+    flowcount = 0
+    for path, img, im0s, vid_cap in dataset:
+        t = time.time()
+
+        # Get detections
+        img = torch.from_numpy(img).to(device)
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        if flowcount == 0 :
+            lastimg = img
+            lastshow = im0s
+            flowcount = 1
+            continue
+
+        #### TODO: calculate detections
+        pred = model(lastimg)[0]
+
+        #### TODO: calculate flows
+        flows_esti = torch.cat([lastimg, img], 1)
+        wholeflows = batch_estimate(flows_esti, LiteFlowNet)
+
+        if opt.half:
+            pred = pred.float()
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0 = path[i], '%g: ' % i, im0s[i]
+            else:
+                p, s, im0 = path, '', lastshow
+
+            save_path = str(Path(out) / Path(p).name)
+            s += '%gx%g ' % img.shape[2:]  # print string
+            if det is not None and len(det):
+                # Print results
+                for c in det[:, -1].unique():
+                    if c != 0:
+                        continue
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                for *xyxy, conf, cls in det:
+                    if cls != 0:
+                        continue
+
+                    xmin, ymin, xmax, ymax = max(int(xyxy[0]), 0), max(int(xyxy[1]), 0), min(int(xyxy[2]), 512), min(int(xyxy[3]), 512)
+                    flows = wholeflows[0, :, :, :].permute(1, 2, 0)
+                    person_H, person_W = ymax - ymin, xmax - xmin
+                    box_ratio = person_H / person_W
+
+                    person_flows = flows[ymin:ymax, xmin:xmax, :]
+                    u = person_flows[:, :, 0].cpu().numpy()
+                    v = person_flows[:, :, 1].cpu().numpy()
+
+                    rad = np.sqrt(np.square(u) + np.square(v))
+                    rad_max = np.max(rad)
+                    epsilon = 1e-5
+                    u = u / (rad_max + epsilon)
+                    v = v / (rad_max + epsilon)
+                    rad = np.sqrt(np.square(u) + np.square(v))
+
+                    radis = torch.from_numpy(np.arctan2(-v, -u))
+                    left_radis = -130 * np.pi / 180
+                    right_radis = -45 * np.pi / 180
+                    downnum = (radis[(radis > left_radis) & (radis < right_radis)]).numel()
+                    total_num = radis.numel()
+                    downpixel_ratio = downnum / total_num
+
+                    person_flows_mean = rad.mean()
+
+
+                    if  box_ratio <1.8 and downpixel_ratio > 0.7 :
+                        label = 'fall down'
+                        color = [0, 0, 255]
+
+                    else :
+                        label = 'normal'
+                        color = [0, 255, 0]
+
+
+                    plot_one_box(xyxy, im0, label=label, color=color)
+
+            lastimg = img
+            lastshow = im0s
+
+            # Print time (inference + NMS)
+            print('%sDone. (%.3fs)' % (s, time.time() - t))
+
+            # Stream results
+            if view_img:
+                cv2.imshow(p, im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'images':
+                    cv2.imwrite(save_path, im0)
+                else:
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+
+                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*opt.fourcc), fps, (w, h))
+                    vid_writer.write(im0)
+
+    if save_txt or save_img:
+        print('Results saved to %s' % os.getcwd() + os.sep + out)
+        if platform == 'darwin':  # MacOS
+            os.system('open ' + out + ' ' + save_path)
+
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default='cfg/yolov3.cfg', help='*.cfg path')
+    parser.add_argument('--names', type=str, default='data/coco.names', help='*.names path')
+    parser.add_argument('--weights', type=str, default='weights/yolov3.pt', help='path to weights file')
+    parser.add_argument('--source', type=str, default='data/samples/falldown.avi', help='source')  # input file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
+    parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
+    parser.add_argument('--view-img', action='store_true', default=True, help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--LiteFlowNet_weights', type=str, default='weights/LiteFlowNet.pt', help='path to LiteFlowNet weights')
+    opt = parser.parse_args()
+    print(opt)
+
+    with torch.no_grad():
+        detect()
